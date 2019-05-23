d528a8a724f36ce92ee6a3980ba4e1953549de64
GQSM <ms314006@hotmail.com>
2019-05-24 07:06:36 +0800

    [CHANGE] 將 fetch 請求的部分改成以 call 的特性呼叫

--- Ch01/src/saga/index.js
+++ Ch01/src/saga/index.js
@@ -2,10 +2,14 @@ import { call, put, takeEvery } from 'redux-saga/effects';
 import { FETCH_DATA, FETCH_DATA_SUCCESS } from '../reducer';
 
 function* fetchData() {
+  const response = yield call(fetch, 'https://httpbin.org/get');
+  const data = yield response.json();
+  /*
   const data = yield call(
     () => fetch('https://httpbin.org/get')
       .then(response => response.json()),
   );
+  */
   yield put({ type: FETCH_DATA_SUCCESS, payload: { data } });
 }
 
